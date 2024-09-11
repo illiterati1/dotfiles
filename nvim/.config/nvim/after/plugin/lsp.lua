11d3643146d4d1fbfc525fa569f780fcb681baa8
Soiarse <brendan.x.wilson@gmail.com>
2024-09-11 15:15:26 -0500

    removing rust-tools and neorg

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -16,18 +16,6 @@ lsp.skip_server_setup('rust_analyzer')
 
 lsp.setup()
 
----- rust-tools
-local rt = require('rust-tools')
-
-rt.setup({
-  server = {
-    on_attach = function(_, bufnr)
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr})
-      vim.keymap.set("n", "<S-Space>", rt.hover_actions.hover_actions, { buffer = bufnr})
-    end
-  },
-})
-
 ---- cmp
 local cmp = require('cmp')
 
